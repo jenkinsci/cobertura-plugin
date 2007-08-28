b231aec97a6118694822b6f81109520550da5dd8
null <null@dev.java.net>
2007-08-28 15:00:20 +0000

    This commit was manufactured by cvs2svn to create tag 'cobertura-0_3'.

---
+++ hudson/plugins/cobertura/src/main/java/hudson/plugins/cobertura/targets/CoverageResult.java
@@ -0,0 +1,305 @@
+package hudson.plugins.cobertura.targets;
+
+import hudson.plugins.cobertura.Ratio;
+import hudson.plugins.cobertura.CoberturaBuildAction;
+import hudson.model.Build;
+import hudson.model.Run;
+import hudson.util.ChartUtil;
+import hudson.util.DataSetBuilder;
+import hudson.util.ShiftedCategoryAxis;
+import hudson.util.ColorPalette;
+
+import java.util.*;
+import java.util.List;
+import java.io.IOException;
+import java.awt.*;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.ui.RectangleEdge;
+import org.jfree.ui.RectangleInsets;
+
+/**
+ * TODO javadoc.
+ *
+ * @author Stephen Connolly
+ * @since 22-Aug-2007 18:47:10
+ */
+public class CoverageResult {
+    private final CoverageElement element;
+    private final Map<CoverageMetric, Ratio> aggregateResults = new HashMap<CoverageMetric, Ratio>();
+    private final Map<CoverageMetric, Ratio> localResults = new HashMap<CoverageMetric, Ratio>();
+    private final CoverageResult parent;
+    private final Map<String, CoverageResult> children = new HashMap<String, CoverageResult>();
+    private final String name;
+    public Build owner = null;
+
+    public CoverageResult(CoverageElement elementType, CoverageResult parent, String name) {
+        this.element = elementType;
+        this.parent = parent;
+        this.name = name;
+        if (this.parent != null) {
+            this.parent.children.put(name, this);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CoverageResult getParent() {
+        return parent;
+    }
+
+    public CoverageElement getElement() {
+        return element;
+    }
+
+    public List<CoverageResult> getParents() {
+        List<CoverageResult> result = new ArrayList<CoverageResult>();
+        CoverageResult p = getParent();
+        while (p != null) {
+            result.add(p);
+            p = p.getParent();
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    public Set<CoverageElement> getChildElements() {
+        Set<CoverageElement> result = new HashSet<CoverageElement>();
+        for (CoverageResult child : children.values()) {
+            result.add(child.element);
+        }
+        return result;
+    }
+
+    public Set<String> getChildren(CoverageElement element) {
+        Set<String> result = new TreeSet<String>();
+        for (CoverageResult child : children.values()) {
+            if (child.element.equals(element)) {
+                result.add(child.name);
+            }
+        }
+        return result;
+    }
+
+    public Set<CoverageMetric> getChildMetrics(CoverageElement element) {
+        Set<CoverageMetric> result = new TreeSet<CoverageMetric>();
+        for (CoverageResult child : children.values()) {
+            if (child.element.equals(element)) {
+                result.addAll(child.getMetrics());
+            }
+        }
+        return result;
+    }
+
+    public Set<String> getChildren() {
+        return children.keySet();
+    }
+
+    public Map<CoverageMetric, Ratio> getResults() {
+        return Collections.unmodifiableMap(aggregateResults);
+    }
+
+    public String urlTransform(String name) {
+        StringBuilder buf = new StringBuilder(name.length());
+        for (int i = 0; i < name.length(); i++) {
+            final char c = name.charAt(i);
+            if (('0' <= c && '9' >= c)
+                    || ('A' <= c && 'Z' >= c)
+                    || ('a' <= c && 'z' >= c)) {
+                buf.append(c);
+            } else {
+                buf.append('_');
+            }
+        }
+        return buf.toString();
+    }
+
+    public String xmlTransform(String name) {
+        return name.replaceAll("\\&", "&amp;").replaceAll("\\<", "&lt;").replaceAll("\\>", "&gt;");
+    }
+
+    public String relativeUrl(CoverageResult parent) {
+        StringBuffer url = new StringBuffer("..");
+        CoverageResult p = getParent();
+        while (p != null && p != parent) {
+            url.append("/..");
+            p = p.getParent();
+        }
+        return url.toString();
+    }
+
+    public CoverageResult getChild(String name) {
+        return children.get(name);
+    }
+
+    public Ratio getCoverage(CoverageMetric metric) {
+        return aggregateResults.get(metric);
+    }
+
+    public Set<CoverageMetric> getMetrics() {
+        return Collections.unmodifiableSet(new TreeSet(aggregateResults.keySet()));
+    }
+
+    public void updateMetric(CoverageMetric metric, Ratio additionalResult) {
+        if (localResults.containsKey(metric)) {
+            Ratio existingResult = localResults.get(metric);
+            localResults.put(metric, CoverageAggreagtionRule.combine(metric, existingResult, additionalResult));
+        } else {
+            localResults.put(metric, additionalResult);
+        }
+    }
+
+    /**
+     * Getter for property 'owner'.
+     *
+     * @return Value for property 'owner'.
+     */
+    public Build getOwner() {
+        return owner;
+    }
+
+    /**
+     * Setter for property 'owner'.
+     *
+     * @param owner Value to set for property 'owner'.
+     */
+    public void setOwner(Build owner) {
+        this.owner = owner;
+        aggregateResults.clear();
+        for (CoverageResult child : children.values()) {
+            child.setOwner(owner);
+            for (Map.Entry<CoverageMetric, Ratio> childResult : child.aggregateResults.entrySet()) {
+                aggregateResults.putAll(CoverageAggreagtionRule.aggregate(child.getElement(),
+                        childResult.getKey(), childResult.getValue(), aggregateResults));
+            }
+        }
+        // override any local results (as they should be more accurate than the aggregated ones)
+        aggregateResults.putAll(localResults);
+    }
+
+    /**
+     * Getter for property 'previousResult'.
+     *
+     * @return Value for property 'previousResult'.
+     */
+    public CoverageResult getPreviousResult() {
+        if (parent == null) {
+            if (owner == null) {
+                return null;
+            }
+            Run prevBuild = owner.getPreviousBuild();
+            if (prevBuild == null) {
+                return null;
+            }
+            CoberturaBuildAction action = prevBuild.getAction(CoberturaBuildAction.class);
+            if (action == null) {
+                return null;
+            }
+            return action.getResult();
+        } else {
+            CoverageResult prevParent = parent.getPreviousResult();
+            return prevParent == null ? null : prevParent.getChild(name);
+        }
+    }
+
+    public Object getDynamic(String token, StaplerRequest req, StaplerResponse rsp) throws IOException {
+        token = token.toLowerCase();
+        for (String name : children.keySet()) {
+            if (urlTransform(name).toLowerCase().equals(token)) {
+                return getChild(name);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Generates the graph that shows the coverage trend up to this report.
+     */
+    public void doGraph(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        if (ChartUtil.awtProblem) {
+            // not available. send out error message
+            rsp.sendRedirect2(req.getContextPath() + "/images/headless.png");
+            return;
+        }
+
+        Build build = getOwner();
+        Calendar t = build.getTimestamp();
+
+        if (req.checkIfModified(t, rsp))
+            return; // up to date
+
+        DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dsb = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
+
+        for (CoverageResult a = this; a != null; a = a.getPreviousResult()) {
+            ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(a.getOwner());
+            for (Map.Entry<CoverageMetric, Ratio> value : a.aggregateResults.entrySet()) {
+                dsb.add(value.getValue().getPercentageFloat(), value.getKey().getName(), label);
+            }
+        }
+
+        ChartUtil.generateGraph(req, rsp, createChart(dsb.build()), 400, 200);
+    }
+
+    private JFreeChart createChart(CategoryDataset dataset) {
+
+        final JFreeChart chart = ChartFactory.createLineChart(
+                null,                   // chart title
+                null,                   // unused
+                "%",                    // range axis label
+                dataset,                  // data
+                PlotOrientation.VERTICAL, // orientation
+                true,                     // include legend
+                true,                     // tooltips
+                false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        final LegendTitle legend = chart.getLegend();
+        legend.setPosition(RectangleEdge.RIGHT);
+
+        chart.setBackgroundPaint(Color.white);
+
+        final CategoryPlot plot = chart.getCategoryPlot();
+
+        // plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setOutlinePaint(null);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.black);
+
+        CategoryAxis domainAxis = new ShiftedCategoryAxis(null);
+        plot.setDomainAxis(domainAxis);
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        domainAxis.setLowerMargin(0.0);
+        domainAxis.setUpperMargin(0.0);
+        domainAxis.setCategoryMargin(0.0);
+
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setUpperBound(100);
+        rangeAxis.setLowerBound(0);
+
+        final LineAndShapeRenderer renderer = (LineAndShapeRenderer) plot.getRenderer();
+        renderer.setStroke(new BasicStroke(2.0f));
+        ColorPalette.apply(renderer);
+
+        // crop extra space around the graph
+        plot.setInsets(new RectangleInsets(5.0, 0, 0, 5.0));
+
+        return chart;
+    }
+}
