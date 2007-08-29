16aaf22fa594422b7a284ab71dfecfbbfeee943f
null <null@dev.java.net>
2007-08-29 14:38:17 +0000

    This commit was manufactured by cvs2svn to create tag 'cobertura-0_4'.

---
+++ hudson/plugins/cobertura/src/main/java/hudson/plugins/cobertura/CoberturaBuildAction.java
@@ -0,0 +1,243 @@
+package hudson.plugins.cobertura;
+
+import hudson.model.Build;
+import hudson.model.HealthReport;
+import hudson.model.HealthReportingAction;
+import hudson.model.Result;
+import hudson.plugins.cobertura.targets.CoverageMetric;
+import hudson.plugins.cobertura.targets.CoverageTarget;
+import hudson.plugins.cobertura.targets.CoverageResult;
+import hudson.util.ChartUtil;
+import hudson.util.DataSetBuilder;
+import hudson.util.ShiftedCategoryAxis;
+import hudson.util.ColorPalette;
+import org.kohsuke.stapler.StaplerProxy;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.ui.RectangleEdge;
+import org.jfree.ui.RectangleInsets;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author connollys
+ * @since 03-Jul-2007 08:43:08
+ */
+public class CoberturaBuildAction implements HealthReportingAction, StaplerProxy {
+    public final Build owner;
+    private CoverageTarget healthyTarget;
+    private CoverageTarget unhealthyTarget;
+    private Map<CoverageMetric, Ratio> result;
+
+    private transient WeakReference<CoverageResult> report;
+
+    public HealthReport getBuildHealth() {
+        if (healthyTarget == null || unhealthyTarget == null) return null;
+        if (result == null) {
+            CoverageResult projectCoverage = getResult();
+            result = new HashMap<CoverageMetric, Ratio>();
+            result.putAll(projectCoverage.getResults());
+        }
+        Map<CoverageMetric, Integer> scores = healthyTarget.getRangeScores(unhealthyTarget, result);
+        int minValue = 100;
+        CoverageMetric minKey = null;
+        for (Map.Entry<CoverageMetric, Integer> e : scores.entrySet()) {
+            if (e.getValue() < minValue) {
+                minKey = e.getKey();
+                minValue = e.getValue();
+            }
+        }
+        if (minKey == null) return null;
+
+        StringBuilder description = new StringBuilder("Cobertura Coverage: ");
+        description.append(minKey.getName());
+        description.append(" ");
+        description.append(result.get(minKey).getPercentage());
+        description.append("% (");
+        description.append(result.get(minKey).toString());
+        description.append(")");
+        return new HealthReport(minValue, description.toString());
+    }
+
+    public String getIconFileName() {
+        return "graph.gif";  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String getDisplayName() {
+        return "Coverage Report";  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String getUrlName() {
+        return "cobertura";  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Object getTarget() {
+        return getResult();  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public CoberturaBuildAction getPreviousResult() {
+        return getPreviousResult(owner);
+    }
+
+    /**
+     * Gets the previous {@link CoberturaBuildAction} of the given build.
+     */
+    /*package*/
+    static CoberturaBuildAction getPreviousResult(Build start) {
+        Build<?, ?> b = start;
+        while (true) {
+            b = b.getPreviousBuild();
+            if (b == null)
+                return null;
+            if (b.getResult() == Result.FAILURE)
+                continue;
+            CoberturaBuildAction r = b.getAction(CoberturaBuildAction.class);
+            if (r != null)
+                return r;
+        }
+    }
+
+    CoberturaBuildAction(Build owner, CoverageResult r, CoverageTarget healthyTarget,
+                         CoverageTarget unhealthyTarget) {
+        this.owner = owner;
+        this.report = new WeakReference<CoverageResult>(r);
+        this.healthyTarget = healthyTarget;
+        this.unhealthyTarget = unhealthyTarget;
+        r.setOwner(owner);
+        if (result == null) {
+            result = new HashMap<CoverageMetric, Ratio>();
+            result.putAll(r.getResults());
+        }
+    }
+
+
+    /**
+     * Obtains the detailed {@link hudson.plugins.cobertura.targets.CoverageResult} instance.
+     */
+    public synchronized CoverageResult getResult() {
+        if (report != null) {
+            CoverageResult r = report.get();
+            if (r != null) return r;
+        }
+
+        CoverageResult r = null;
+        for (File reportFile : CoberturaPublisher.getCoberturaReports(owner)) {
+            try {
+                r = CoberturaCoverageParser.parse(reportFile, r);
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Failed to load " + reportFile, e);
+            }
+        }
+        if (r != null) {
+            r.setOwner(owner);
+            report = new WeakReference<CoverageResult>(r);
+            return r;
+        } else {
+            return null;
+        }
+    }
+
+    private static final Logger logger = Logger.getLogger(CoberturaBuildAction.class.getName());
+
+    public static CoberturaBuildAction load(Build<?, ?> build, CoverageResult result, CoverageTarget healthyTarget,
+                                            CoverageTarget unhealthyTarget) {
+        return new CoberturaBuildAction(build, result, healthyTarget, unhealthyTarget);
+    }
+
+    /**
+     * Generates the graph that shows the coverage trend up to this report.
+     */
+    public void doGraph(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        if (ChartUtil.awtProblem) {
+            // not available. send out error message
+            rsp.sendRedirect2(req.getContextPath() + "/images/headless.png");
+            return;
+        }
+
+        Calendar t = owner.getTimestamp();
+
+        if (req.checkIfModified(t, rsp))
+            return; // up to date
+
+        DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel> dsb = new DataSetBuilder<String, ChartUtil.NumberOnlyBuildLabel>();
+
+        for (CoberturaBuildAction a = this; a != null; a = a.getPreviousResult()) {
+            ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(a.owner);
+            for (Map.Entry<CoverageMetric, Ratio> value : a.result.entrySet()) {
+                dsb.add(value.getValue().getPercentageFloat(), value.getKey().getName(), label);
+            }
+        }
+
+        ChartUtil.generateGraph(req, rsp, createChart(dsb.build()), 400, 200);
+    }
+
+    private JFreeChart createChart(CategoryDataset dataset) {
+
+        final JFreeChart chart = ChartFactory.createLineChart(
+                null,                   // chart title
+                null,                   // unused
+                "%",                    // range axis label
+                dataset,                  // data
+                PlotOrientation.VERTICAL, // orientation
+                true,                     // include legend
+                true,                     // tooltips
+                false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        final LegendTitle legend = chart.getLegend();
+        legend.setPosition(RectangleEdge.RIGHT);
+
+        chart.setBackgroundPaint(Color.white);
+
+        final CategoryPlot plot = chart.getCategoryPlot();
+
+        // plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setOutlinePaint(null);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.black);
+
+        CategoryAxis domainAxis = new ShiftedCategoryAxis(null);
+        plot.setDomainAxis(domainAxis);
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        domainAxis.setLowerMargin(0.0);
+        domainAxis.setUpperMargin(0.0);
+        domainAxis.setCategoryMargin(0.0);
+
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setUpperBound(100);
+        rangeAxis.setLowerBound(0);
+
+        final LineAndShapeRenderer renderer = (LineAndShapeRenderer) plot.getRenderer();
+        renderer.setStroke(new BasicStroke(2.0f));
+        ColorPalette.apply(renderer);
+
+        // crop extra space around the graph
+        plot.setInsets(new RectangleInsets(5.0, 0, 0, 5.0));
+
+        return chart;
+    }
+}
---
+++ hudson/plugins/cobertura/src/main/java/hudson/plugins/cobertura/CoberturaCoverageParser.java
@@ -0,0 +1,333 @@
+package hudson.plugins.cobertura;
+
+import hudson.plugins.cobertura.targets.CoverageResult;
+import hudson.plugins.cobertura.targets.CoverageElement;
+import hudson.plugins.cobertura.targets.CoverageMetric;
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.Stack;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author connollys
+ * @since 03-Jul-2007 09:03:30
+ */
+public class CoberturaCoverageParser {
+
+    /**
+     * Do not instantiate CoberturaCoverageParser.
+     */
+    private CoberturaCoverageParser() {
+    }
+
+    public static CoverageResult parse(File inFile, CoverageResult cumulative) throws IOException {
+        FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(inFile);
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+            return parse(bufferedInputStream, cumulative);
+        } finally {
+            try {
+                if (bufferedInputStream != null)
+                    bufferedInputStream.close();
+                if (fileInputStream != null)
+                    fileInputStream.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public static CoverageResult parse(InputStream in, CoverageResult cumulative) throws IOException {
+        if (in == null) throw new NullPointerException();
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(false);
+        try {
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        } catch (ParserConfigurationException e) {
+        } catch (SAXNotRecognizedException e) {
+        } catch (SAXNotSupportedException e) {
+        }
+        try {
+            SAXParser parser = factory.newSAXParser();
+            CoberturaXmlHandler handler = new CoberturaXmlHandler(cumulative);
+            parser.parse(in, handler);
+            return handler.getRootCoverage();
+        } catch (ParserConfigurationException e) {
+            throw new IOException("Cannot parse coverage results", e);
+        } catch (SAXException e) {
+            throw new IOException("Cannot parse coverage results", e);
+        }
+    }
+}
+
+class CoberturaXmlHandler extends DefaultHandler {
+    private CoverageResult rootCoverage;
+    private Stack<CoverageResult> stack = new Stack<CoverageResult>();
+    private static final String DEFAULT_PACKAGE = "<default>";
+
+    public CoberturaXmlHandler(CoverageResult rootCoverage) {
+        this.rootCoverage = rootCoverage;
+    }
+
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        if (this.rootCoverage == null) {
+            this.rootCoverage = new CoverageResult(CoverageElement.PROJECT, null, "Cobertura Coverage Report");
+        }
+        stack.clear();
+    }
+
+    public void endDocument() throws SAXException {
+        if (!stack.empty()) {
+            throw new SAXException("Unbalanced parse of cobertua coverage results.");
+        }
+        super.endDocument();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    private void descend(CoverageElement childType, String childName) {
+        CoverageResult child = rootCoverage.getChild(childName);
+        stack.push(rootCoverage);
+        if (child == null) {
+            rootCoverage = new CoverageResult(childType, rootCoverage, childName);
+        } else {
+            rootCoverage = child;
+        }
+    }
+
+    private void ascend(CoverageElement element) {
+        while (rootCoverage != null && rootCoverage.getElement() != element) {
+            rootCoverage = stack.pop();
+        }
+        if (rootCoverage != null) {
+            rootCoverage = stack.pop();
+        }
+    }
+
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        String name = attributes.getValue("name");
+        if ("coverage".equals(qName)) {
+        } else if ("package".equals(qName)) {
+            if ("".equals(name) || null == name) {
+                name = DEFAULT_PACKAGE;
+            }
+            descend(CoverageElement.JAVA_PACKAGE, name);
+        } else if ("class".equals(qName)) {
+            assert rootCoverage.getElement() == CoverageElement.JAVA_PACKAGE;
+            // cobertura combines file and class
+            String filename = attributes.getValue("filename").replace('\\', '/');
+            String packageName = rootCoverage.getName();
+            if (!DEFAULT_PACKAGE.equals(packageName)) {
+                if (filename.startsWith(packageName.replace('.', '/') + "/")) {
+                    filename = filename.substring(packageName.length() + 1);
+                }
+            }
+            if (name.startsWith(packageName + ".")) {
+                name = name.substring(packageName.length() + 1);
+            }
+            descend(CoverageElement.JAVA_FILE, filename);
+            descend(CoverageElement.JAVA_CLASS, name);
+        } else if ("method".equals(qName)) {
+            String methodName = buildMethodName(name, attributes.getValue("signature"));
+            descend(CoverageElement.JAVA_METHOD, methodName);
+        } else if ("line".equals(qName)) {
+            String hitsString = attributes.getValue("hits");
+            try {
+                int hits = Integer.parseInt(hitsString);
+                rootCoverage.updateMetric(CoverageMetric.LINE, Ratio.create((hits == 0) ? 0 : 1, 1));
+            } catch (NumberFormatException e) {
+                // ignore
+            }
+            if (Boolean.parseBoolean(attributes.getValue("branch"))) {
+                final String conditionCoverage = attributes.getValue("condition-coverage");
+                if (conditionCoverage != null) {
+                    // some cases in the wild have branch = true but no condition-coverage attribute
+
+                    // should be of the format xxx% (yyy/zzz)
+                    Matcher matcher = Pattern.compile("(\\d*)\\%\\s*\\((\\d*)/(\\d*)\\)").matcher(conditionCoverage);
+                    if (matcher.matches()) {
+                        assert matcher.groupCount() == 3;
+                        final String numeratorStr = matcher.group(2);
+                        final String denominatorStr = matcher.group(3);
+                        try {
+                            rootCoverage.updateMetric(CoverageMetric.CONDITIONAL,
+                                    Ratio.create(Integer.parseInt(numeratorStr), Integer.parseInt(denominatorStr)));
+                        } catch (NumberFormatException e) {
+                            // ignore
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private String buildMethodName(String name, String signature) {
+        Matcher signatureMatcher = Pattern.compile("\\((.*)\\)(.*)").matcher(signature);
+        StringBuilder methodName = new StringBuilder();
+        if (signatureMatcher.matches()) {
+            Pattern argMatcher = Pattern.compile("\\[*([TL][^\\;]*\\;)|([ZCBSIFJDV])");
+            String returnType = signatureMatcher.group(2);
+            Matcher matcher = argMatcher.matcher(returnType);
+            if (matcher.matches()) {
+                methodName.append(parseMethodArg(matcher.group()));
+                methodName.append(' ');
+            }
+            methodName.append(name);
+            String args = signatureMatcher.group(1);
+            matcher = argMatcher.matcher(args);
+            methodName.append('(');
+            boolean first = true;
+            while (matcher.find()) {
+                if (!first) {
+                    methodName.append(',');
+                }
+                methodName.append(parseMethodArg(matcher.group()));
+                first = false;
+            }
+            methodName.append(')');
+        } else {
+            methodName.append(name);
+        }
+        return methodName.toString();
+    }
+
+    private String parseMethodArg(String s) {
+        char c = s.charAt(0);
+        int end;
+        switch (c) {
+            case'Z':
+                return "boolean";
+            case'C':
+                return "char";
+            case'B':
+                return "byte";
+            case'S':
+                return "short";
+            case'I':
+                return "int";
+            case'F':
+                return "float";
+            case'J':
+                return "";
+            case'D':
+                return "double";
+            case'V':
+                return "void";
+            case'[':
+                return parseMethodArg(s.substring(1)) + "[]";
+            case'T':
+            case'L':
+                end = s.indexOf(';');
+                return s.substring(1, end).replace('/', '.');
+        }
+        return s;
+    }
+
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if ("coverage".equals(qName)) {
+        } else if ("package".equals(qName)) {
+            ascend(CoverageElement.JAVA_PACKAGE);
+        } else if ("class".equals(qName)) {
+            ascend(CoverageElement.JAVA_CLASS);
+            ascend(CoverageElement.JAVA_FILE);
+        } else if ("method".equals(qName)) {
+            ascend(CoverageElement.JAVA_METHOD);
+        }
+        super.endElement(uri, localName, qName);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public CoverageResult getRootCoverage() {
+        return rootCoverage;
+    }
+}
+
+class CoberturaXmlHandlerStackItem {
+    private CoverageResult metric;
+    private CoberturaCoverageTotals totals;
+
+
+    public CoberturaXmlHandlerStackItem(CoverageResult metric) {
+        this.metric = metric;
+        totals = new CoberturaCoverageTotals();
+    }
+
+    public CoverageResult getMetric() {
+        return metric;
+    }
+
+    public CoberturaCoverageTotals getTotals() {
+        return totals;
+    }
+}
+
+class CoberturaCoverageTotals {
+    private long totalLineCount;
+    private long coverLineCount;
+    private long totalConditionCount;
+    private long coverConditionCount;
+    private long totalMethodCount;
+    private long coverMethodCount;
+
+    public CoberturaCoverageTotals() {
+        totalLineCount = 0;
+        coverLineCount = 0;
+        totalConditionCount = 0;
+        coverConditionCount = 0;
+        totalMethodCount = 0;
+        coverMethodCount = 0;
+    }
+
+    public void addLine(boolean covered) {
+        totalLineCount++;
+        if (covered) {
+            coverLineCount++;
+        }
+    }
+
+    public void addMethod(boolean covered) {
+        totalMethodCount++;
+        if (covered) {
+            coverMethodCount++;
+        }
+    }
+
+    public void addLine(boolean covered, int condCoverCount, int condTotalCount) {
+        addLine(covered);
+        totalConditionCount += condTotalCount;
+        coverConditionCount += condCoverCount;
+    }
+
+    public void addTotal(CoberturaCoverageTotals sub) {
+        totalLineCount += sub.totalLineCount;
+        coverLineCount += sub.coverLineCount;
+        totalConditionCount += sub.totalConditionCount;
+        coverConditionCount += sub.coverConditionCount;
+        totalMethodCount += sub.totalMethodCount;
+        coverMethodCount += sub.coverMethodCount;
+    }
+
+    public Ratio getLineCoverage() {
+        return Ratio.create(coverLineCount, totalLineCount);
+    }
+
+    public Ratio getConditionalCoverage() {
+        return Ratio.create(coverLineCount, totalLineCount);
+    }
+
+    public Ratio getMethodCoverage() {
+        return Ratio.create(coverMethodCount, totalMethodCount);
+    }
+}
