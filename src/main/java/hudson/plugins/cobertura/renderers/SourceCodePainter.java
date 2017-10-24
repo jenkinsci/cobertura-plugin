decc745c0f1c3d565d0d80a921f8778dc9f259db
beamerblvd <github@nicholaswilliams.net>
2017-10-24 08:26:23 -0500

    Add total line count to coverage paint for programmatic usage

--- src/main/java/hudson/plugins/cobertura/renderers/SourceCodePainter.java
+++ src/main/java/hudson/plugins/cobertura/renderers/SourceCodePainter.java
@@ -99,6 +99,7 @@ public class SourceCodePainter extends MasterToSlaveFileCallable<Boolean> implem
                 output.write("</tr>\n");
             }
 
+            paint.setTotalLines(line)
         } finally {
             closeQuietly(output);
             closeQuietly(bos);
