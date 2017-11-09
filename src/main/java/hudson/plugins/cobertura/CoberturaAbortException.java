9dfe7d79e911131ffc21974ce7002ac3e71926b8
Jeff Pearce <jxpearce@godaddy.com>
2017-11-09 08:45:30 -0800

    Update comment for CoberturaAbortException

--- src/main/java/hudson/plugins/cobertura/CoberturaAbortException.java
+++ src/main/java/hudson/plugins/cobertura/CoberturaAbortException.java
@@ -3,12 +3,17 @@ package hudson.plugins.cobertura;
 import hudson.AbortException;
 
 /**
- *
- * @author jeffpearce
+ * Common abort exception that should be thrown to abort the build. Prepends
+ * a common prefix to the abort message.
  */
 public class CoberturaAbortException extends AbortException {
+  /**
+   * CoberturaAbortException constructor
+   * @param  String message       the abort message
+   * @return        CoberturaAbortException 
+   */
     public CoberturaAbortException(String message) {
         super("[Cobertura] " + message);
     }
-    
+
 }
