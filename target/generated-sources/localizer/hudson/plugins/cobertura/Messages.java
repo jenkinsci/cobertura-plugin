5a8667e3c8ada7c6129c4360ee0ec1e2860bdade
MaxReinerFullStack <max.reiner@gmx.at>
2017-11-30 00:44:52 +0100

    Cobertura plugin recompiled with 'optional=true'-switch to @Extension

---
+++ target/generated-sources/localizer/hudson/plugins/cobertura/Messages.java
@@ -0,0 +1,326 @@
+// CHECKSTYLE:OFF
+
+package hudson.plugins.cobertura;
+
+import org.jvnet.localizer.Localizable;
+import org.jvnet.localizer.ResourceBundleHolder;
+
+
+/**
+ * Generated localization support class.
+ * 
+ */
+@SuppressWarnings({
+    "",
+    "PMD",
+    "all"
+})
+public class Messages {
+
+    /**
+     * The resource bundle reference
+     * 
+     */
+    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
+
+    /**
+     * Key {@code CoverageColumn.columnName}: {@code Cobertura coverage}.
+     * 
+     * @return
+     *     {@code Cobertura coverage}
+     */
+    public static String CoverageColumn_columnName() {
+        return holder.format("CoverageColumn.columnName");
+    }
+
+    /**
+     * Key {@code CoverageColumn.columnName}: {@code Cobertura coverage}.
+     * 
+     * @return
+     *     {@code Cobertura coverage}
+     */
+    public static Localizable _CoverageColumn_columnName() {
+        return new Localizable(holder, "CoverageColumn.columnName");
+    }
+
+    /**
+     * Key {@code CoverageColumn.line}: {@code {0}%}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code {0}%}
+     */
+    public static String CoverageColumn_line(Object arg0) {
+        return holder.format("CoverageColumn.line", arg0);
+    }
+
+    /**
+     * Key {@code CoverageColumn.line}: {@code {0}%}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code {0}%}
+     */
+    public static Localizable _CoverageColumn_line(Object arg0) {
+        return new Localizable(holder, "CoverageColumn.line", arg0);
+    }
+
+    /**
+     * Key {@code CoberturaProjectAction.displayName}: {@code Coverage
+     * Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static String CoberturaProjectAction_displayName() {
+        return holder.format("CoberturaProjectAction.displayName");
+    }
+
+    /**
+     * Key {@code CoberturaProjectAction.displayName}: {@code Coverage
+     * Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static Localizable _CoberturaProjectAction_displayName() {
+        return new Localizable(holder, "CoberturaProjectAction.displayName");
+    }
+
+    /**
+     * Key {@code CoverageColumn.empty}: {@code -}.
+     * 
+     * @return
+     *     {@code -}
+     */
+    public static String CoverageColumn_empty() {
+        return holder.format("CoverageColumn.empty");
+    }
+
+    /**
+     * Key {@code CoverageColumn.empty}: {@code -}.
+     * 
+     * @return
+     *     {@code -}
+     */
+    public static Localizable _CoverageColumn_empty() {
+        return new Localizable(holder, "CoverageColumn.empty");
+    }
+
+    /**
+     * Key {@code CoverageColumn.columnHeader}: {@code Coverage %}.
+     * 
+     * @return
+     *     {@code Coverage %}
+     */
+    public static String CoverageColumn_columnHeader() {
+        return holder.format("CoverageColumn.columnHeader");
+    }
+
+    /**
+     * Key {@code CoverageColumn.columnHeader}: {@code Coverage %}.
+     * 
+     * @return
+     *     {@code Coverage %}
+     */
+    public static Localizable _CoverageColumn_columnHeader() {
+        return new Localizable(holder, "CoverageColumn.columnHeader");
+    }
+
+    /**
+     * Key {@code CoberturaBuildAction.displayName}: {@code Coverage Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static String CoberturaBuildAction_displayName() {
+        return holder.format("CoberturaBuildAction.displayName");
+    }
+
+    /**
+     * Key {@code CoberturaBuildAction.displayName}: {@code Coverage Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static Localizable _CoberturaBuildAction_displayName() {
+        return new Localizable(holder, "CoberturaBuildAction.displayName");
+    }
+
+    /**
+     * Key {@code CoberturaCoverageParser.name}: {@code Cobertura Coverage
+     * Report}.
+     * 
+     * @return
+     *     {@code Cobertura Coverage Report}
+     */
+    public static String CoberturaCoverageParser_name() {
+        return holder.format("CoberturaCoverageParser.name");
+    }
+
+    /**
+     * Key {@code CoberturaCoverageParser.name}: {@code Cobertura Coverage
+     * Report}.
+     * 
+     * @return
+     *     {@code Cobertura Coverage Report}
+     */
+    public static Localizable _CoberturaCoverageParser_name() {
+        return new Localizable(holder, "CoberturaCoverageParser.name");
+    }
+
+    /**
+     * Key {@code CoberturaBuildAction.description}: {@code Cobertura
+     * Coverage: {0}% ({1}) {2}}.
+     * 
+     * @param arg2
+     *      3rd format parameter, {@code {2}}, as {@link String#valueOf(Object)}.
+     * @param arg1
+     *      2nd format parameter, {@code {1}}, as {@link String#valueOf(Object)}.
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code Cobertura Coverage: {0}% ({1}) {2}}
+     */
+    public static String CoberturaBuildAction_description(Object arg0, Object arg1, Object arg2) {
+        return holder.format("CoberturaBuildAction.description", arg0, arg1, arg2);
+    }
+
+    /**
+     * Key {@code CoberturaBuildAction.description}: {@code Cobertura
+     * Coverage: {0}% ({1}) {2}}.
+     * 
+     * @param arg2
+     *      3rd format parameter, {@code {2}}, as {@link String#valueOf(Object)}.
+     * @param arg1
+     *      2nd format parameter, {@code {1}}, as {@link String#valueOf(Object)}.
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code Cobertura Coverage: {0}% ({1}) {2}}
+     */
+    public static Localizable _CoberturaBuildAction_description(Object arg0, Object arg1, Object arg2) {
+        return new Localizable(holder, "CoberturaBuildAction.description", arg0, arg1, arg2);
+    }
+
+    /**
+     * Key {@code CoverageColumn.branch}: {@code {0}%}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code {0}%}
+     */
+    public static String CoverageColumn_branch(Object arg0) {
+        return holder.format("CoverageColumn.branch", arg0);
+    }
+
+    /**
+     * Key {@code CoverageColumn.branch}: {@code {0}%}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code {0}%}
+     */
+    public static Localizable _CoverageColumn_branch(Object arg0) {
+        return new Localizable(holder, "CoverageColumn.branch", arg0);
+    }
+
+    /**
+     * Key {@code MavenCoberturaAggregatedReport.displayName}: {@code
+     * Coverage Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static String MavenCoberturaAggregatedReport_displayName() {
+        return holder.format("MavenCoberturaAggregatedReport.displayName");
+    }
+
+    /**
+     * Key {@code MavenCoberturaAggregatedReport.displayName}: {@code
+     * Coverage Report}.
+     * 
+     * @return
+     *     {@code Coverage Report}
+     */
+    public static Localizable _MavenCoberturaAggregatedReport_displayName() {
+        return new Localizable(holder, "MavenCoberturaAggregatedReport.displayName");
+    }
+
+    /**
+     * Key {@code MavenCoberturaPublisher.displayName}: {@code Publish
+     * Cobertura Coverage Report}.
+     * 
+     * @return
+     *     {@code Publish Cobertura Coverage Report}
+     */
+    public static String MavenCoberturaPublisher_displayName() {
+        return holder.format("MavenCoberturaPublisher.displayName");
+    }
+
+    /**
+     * Key {@code MavenCoberturaPublisher.displayName}: {@code Publish
+     * Cobertura Coverage Report}.
+     * 
+     * @return
+     *     {@code Publish Cobertura Coverage Report}
+     */
+    public static Localizable _MavenCoberturaPublisher_displayName() {
+        return new Localizable(holder, "MavenCoberturaPublisher.displayName");
+    }
+
+    /**
+     * Key {@code CoberturaPublisher.displayName}: {@code Publish Cobertura
+     * Coverage Report}.
+     * 
+     * @return
+     *     {@code Publish Cobertura Coverage Report}
+     */
+    public static String CoberturaPublisher_displayName() {
+        return holder.format("CoberturaPublisher.displayName");
+    }
+
+    /**
+     * Key {@code CoberturaPublisher.displayName}: {@code Publish Cobertura
+     * Coverage Report}.
+     * 
+     * @return
+     *     {@code Publish Cobertura Coverage Report}
+     */
+    public static Localizable _CoberturaPublisher_displayName() {
+        return new Localizable(holder, "CoberturaPublisher.displayName");
+    }
+
+    /**
+     * Key {@code CoverageColumn.both}: {@code L:{0}% - B:{1}%}.
+     * 
+     * @param arg1
+     *      2nd format parameter, {@code {1}}, as {@link String#valueOf(Object)}.
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code L:{0}% - B:{1}%}
+     */
+    public static String CoverageColumn_both(Object arg0, Object arg1) {
+        return holder.format("CoverageColumn.both", arg0, arg1);
+    }
+
+    /**
+     * Key {@code CoverageColumn.both}: {@code L:{0}% - B:{1}%}.
+     * 
+     * @param arg1
+     *      2nd format parameter, {@code {1}}, as {@link String#valueOf(Object)}.
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code L:{0}% - B:{1}%}
+     */
+    public static Localizable _CoverageColumn_both(Object arg0, Object arg1) {
+        return new Localizable(holder, "CoverageColumn.both", arg0, arg1);
+    }
+
+}
---
+++ target/generated-sources/localizer/hudson/plugins/cobertura/targets/Messages.java
@@ -0,0 +1,246 @@
+// CHECKSTYLE:OFF
+
+package hudson.plugins.cobertura.targets;
+
+import org.jvnet.localizer.Localizable;
+import org.jvnet.localizer.ResourceBundleHolder;
+
+
+/**
+ * Generated localization support class.
+ * 
+ */
+@SuppressWarnings({
+    "",
+    "PMD",
+    "all"
+})
+public class Messages {
+
+    /**
+     * The resource bundle reference
+     * 
+     */
+    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
+
+    /**
+     * Key {@code CoverageElement.Class}: {@code Class}.
+     * 
+     * @return
+     *     {@code Class}
+     */
+    public static String CoverageElement_Class() {
+        return holder.format("CoverageElement.Class");
+    }
+
+    /**
+     * Key {@code CoverageElement.Class}: {@code Class}.
+     * 
+     * @return
+     *     {@code Class}
+     */
+    public static Localizable _CoverageElement_Class() {
+        return new Localizable(holder, "CoverageElement.Class");
+    }
+
+    /**
+     * Key {@code CoverageElement.Method}: {@code Method}.
+     * 
+     * @return
+     *     {@code Method}
+     */
+    public static String CoverageElement_Method() {
+        return holder.format("CoverageElement.Method");
+    }
+
+    /**
+     * Key {@code CoverageElement.Method}: {@code Method}.
+     * 
+     * @return
+     *     {@code Method}
+     */
+    public static Localizable _CoverageElement_Method() {
+        return new Localizable(holder, "CoverageElement.Method");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Packages}: {@code Packages}.
+     * 
+     * @return
+     *     {@code Packages}
+     */
+    public static String CoverageMetrics_Packages() {
+        return holder.format("CoverageMetrics.Packages");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Packages}: {@code Packages}.
+     * 
+     * @return
+     *     {@code Packages}
+     */
+    public static Localizable _CoverageMetrics_Packages() {
+        return new Localizable(holder, "CoverageMetrics.Packages");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Lines}: {@code Lines}.
+     * 
+     * @return
+     *     {@code Lines}
+     */
+    public static String CoverageMetrics_Lines() {
+        return holder.format("CoverageMetrics.Lines");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Lines}: {@code Lines}.
+     * 
+     * @return
+     *     {@code Lines}
+     */
+    public static Localizable _CoverageMetrics_Lines() {
+        return new Localizable(holder, "CoverageMetrics.Lines");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Files}: {@code Files}.
+     * 
+     * @return
+     *     {@code Files}
+     */
+    public static String CoverageMetrics_Files() {
+        return holder.format("CoverageMetrics.Files");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Files}: {@code Files}.
+     * 
+     * @return
+     *     {@code Files}
+     */
+    public static Localizable _CoverageMetrics_Files() {
+        return new Localizable(holder, "CoverageMetrics.Files");
+    }
+
+    /**
+     * Key {@code CoverageElement.Project}: {@code Project}.
+     * 
+     * @return
+     *     {@code Project}
+     */
+    public static String CoverageElement_Project() {
+        return holder.format("CoverageElement.Project");
+    }
+
+    /**
+     * Key {@code CoverageElement.Project}: {@code Project}.
+     * 
+     * @return
+     *     {@code Project}
+     */
+    public static Localizable _CoverageElement_Project() {
+        return new Localizable(holder, "CoverageElement.Project");
+    }
+
+    /**
+     * Key {@code CoverageElement.Package}: {@code Package}.
+     * 
+     * @return
+     *     {@code Package}
+     */
+    public static String CoverageElement_Package() {
+        return holder.format("CoverageElement.Package");
+    }
+
+    /**
+     * Key {@code CoverageElement.Package}: {@code Package}.
+     * 
+     * @return
+     *     {@code Package}
+     */
+    public static Localizable _CoverageElement_Package() {
+        return new Localizable(holder, "CoverageElement.Package");
+    }
+
+    /**
+     * Key {@code CoverageElement.File}: {@code File}.
+     * 
+     * @return
+     *     {@code File}
+     */
+    public static String CoverageElement_File() {
+        return holder.format("CoverageElement.File");
+    }
+
+    /**
+     * Key {@code CoverageElement.File}: {@code File}.
+     * 
+     * @return
+     *     {@code File}
+     */
+    public static Localizable _CoverageElement_File() {
+        return new Localizable(holder, "CoverageElement.File");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Conditionals}: {@code Conditionals}.
+     * 
+     * @return
+     *     {@code Conditionals}
+     */
+    public static String CoverageMetrics_Conditionals() {
+        return holder.format("CoverageMetrics.Conditionals");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Conditionals}: {@code Conditionals}.
+     * 
+     * @return
+     *     {@code Conditionals}
+     */
+    public static Localizable _CoverageMetrics_Conditionals() {
+        return new Localizable(holder, "CoverageMetrics.Conditionals");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Classes}: {@code Classes}.
+     * 
+     * @return
+     *     {@code Classes}
+     */
+    public static String CoverageMetrics_Classes() {
+        return holder.format("CoverageMetrics.Classes");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Classes}: {@code Classes}.
+     * 
+     * @return
+     *     {@code Classes}
+     */
+    public static Localizable _CoverageMetrics_Classes() {
+        return new Localizable(holder, "CoverageMetrics.Classes");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Methods}: {@code Methods}.
+     * 
+     * @return
+     *     {@code Methods}
+     */
+    public static String CoverageMetrics_Methods() {
+        return holder.format("CoverageMetrics.Methods");
+    }
+
+    /**
+     * Key {@code CoverageMetrics.Methods}: {@code Methods}.
+     * 
+     * @return
+     *     {@code Methods}
+     */
+    public static Localizable _CoverageMetrics_Methods() {
+        return new Localizable(holder, "CoverageMetrics.Methods");
+    }
+
+}
