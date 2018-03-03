c612ade943ca91ea43eb2d07d02607ece8666245
ShenYu Zheng <zsy19980307@gmail.com>
2018-03-03 11:07:01 +0800

    JENKINS-43866 Don't round up 99.x% coverage to 100% (#94)

--- src/test/java/hudson/plugins/cobertura/RatioTest.java
+++ src/test/java/hudson/plugins/cobertura/RatioTest.java
@@ -19,6 +19,20 @@ public class RatioTest extends TestCase {
      * @throws Exception
      */
     public void testParseValue() throws Exception {
-        assertRatio(Ratio.create(1,2), 1.0f, 2.0f);
+        assertRatio(Ratio.create(1, 2), 1.0f, 2.0f);
+    }
+
+    public void testGetPercentage() {
+        assertEquals(98, Ratio.create(246, 250).getPercentage());
+        assertEquals(99, Ratio.create(247, 250).getPercentage());
+        assertEquals(99, Ratio.create(248, 250).getPercentage());
+        assertEquals(99, Ratio.create(249, 250).getPercentage());
+        assertEquals(100, Ratio.create(250, 250).getPercentage());
+    }
+
+    public void testGetPercentageFloat() {
+        assertEquals("099.99", Ratio.create(24998, 25000).getPercentageString());
+        assertEquals("099.996", Ratio.create(24999, 25000).getPercentageString());
+        assertEquals("100.000", Ratio.create(25000, 25000).getPercentageString());
     }
 }
