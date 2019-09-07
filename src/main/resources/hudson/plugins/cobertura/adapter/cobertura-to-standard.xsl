ff0472896126aeb93aa5e99772a7237b2538a2cf
Aliaksei Savanchuk <dntsaygoodbye@gmail.com>
2019-09-07 20:11:18 +0300

    Support cases when one file may be in multiple packages
    There is a tool called Coverlet which is a .NET utility that collects
    code coverage information and it produces reports in Cobertura. And this
    Coberura report may contain one file in different packages
    (.NET assemblies). .NET allows you to reference one file in multiple
    projects(assemblies). Because of this, when you use the Cobertura plugin
    to publish your code coverage in Jenkins it shows it incorrect because
    the plugin uses Muenchian grouping which doesn't allow you to have
    one file in multiple packages, it just groups them, globally for a whole
    report. Changed the xslt scheme to use the for-each-group which allows
    to have one file in multiple packages.

--- src/main/resources/hudson/plugins/cobertura/adapter/cobertura-to-standard.xsl
+++ src/main/resources/hudson/plugins/cobertura/adapter/cobertura-to-standard.xsl
@@ -1,8 +1,6 @@
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
     <xsl:output method="xml"/>
-
-    <xsl:key name="filename" match="class" use="@filename"/>
 
     <xsl:template match="/">
         <report>
@@ -28,22 +26,27 @@
             <xsl:attribute name="name">
                 <xsl:value-of select="@name"/>
             </xsl:attribute>
-            <xsl:apply-templates select="classes/class[generate-id(.)=generate-id(key('filename',@filename)[1])]"/>
+            <xsl:apply-templates select="classes"/>
         </package>
     </xsl:template>
 
-    <xsl:template match="classes/class">
-        <file name="{@filename}">
-            <xsl:for-each select="key('filename', @filename)">
-                <class>
-                    <xsl:attribute name="name">
-                        <xsl:value-of select="@name"/>
-                    </xsl:attribute>
-                    <xsl:apply-templates select="methods/method"/>
-                    <xsl:apply-templates select="lines/line"/>
-                </class>
-            </xsl:for-each>
-        </file>
+    <xsl:template match="classes">
+        <xsl:for-each-group select="class" group-by="@filename">
+            <file>
+                <xsl:attribute name="name">
+                    <xsl:value-of select="current-grouping-key()"/>
+                </xsl:attribute>
+                <xsl:for-each select="current-group()">
+                    <class>
+                        <xsl:attribute name="name">
+                            <xsl:value-of select="@name"/>
+                        </xsl:attribute>
+                        <xsl:apply-templates select="methods/method"/>
+                        <xsl:apply-templates select="lines/line"/>
+                    </class>
+                </xsl:for-each>
+            </file>
+        </xsl:for-each-group>
     </xsl:template>
 
     <xsl:template match="methods/method">
---
+++ src/test/java/hudson/plugins/cobertura/adapter/CoberturaReportAdapterTest.java
@@ -0,0 +1,54 @@
+package hudson.plugins.cobertura.adapter;
+
+import hudson.FilePath;
+import hudson.model.Descriptor;
+import hudson.plugins.cobertura.targets.CoverageResult;
+import io.jenkins.plugins.coverage.CoverageAction;
+import io.jenkins.plugins.coverage.adapter.CoverageAdapter;
+import io.jenkins.plugins.coverage.targets.CoverageElement;
+import io.jenkins.plugins.coverage.targets.Ratio;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.Objects;
+
+public class CoberturaReportAdapterTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void XSLT20Test() throws Exception {
+        String coberturaReport = "coberturaWithTheSameFileInDifferentPackages.xml";
+        StringBuilder sb = new StringBuilder();
+        sb.append("node {")
+                .append("publishCoverage(");
+
+        sb.append("adapters:[");
+
+        sb.append(String.format("coberturaAdapter(path: '%s')], sourceFileResolver: sourceFiles('NEVER_STORE')", coberturaReport));
+        sb.append(")").append("}");
+
+        WorkflowJob project = j.createProject(WorkflowJob.class, "coverage-pipeline-test");
+        FilePath workspace = j.jenkins.getWorkspaceFor(project);
+
+        Objects.requireNonNull(workspace)
+                .child(coberturaReport)
+                .copyFrom(getClass().getResourceAsStream(coberturaReport));
+
+        project.setDefinition(new CpsFlowDefinition(sb.toString(), true));
+        WorkflowRun r = Objects.requireNonNull(project.scheduleBuild2(0)).waitForStart();
+        Assert.assertNotNull(r);
+        j.assertBuildStatusSuccess(j.waitForCompletion(r));
+        CoverageAction coverageAction = r.getAction(CoverageAction.class);
+        Assert.assertNotNull(r);
+        Ratio lineCoverage = coverageAction.getResult().getCoverage(CoverageElement.LINE);
+        Assert.assertEquals(lineCoverage.toString(),"6/11");
+    }
+}
